bf15b721efd7e65f120411d26bb0a383af43ac88
Sharv Chaubey <119421573+sharv556@users.noreply.github.com>
2024-08-11 14:45:56 +0530

    first

---
+++ script.js
@@ -0,0 +1,22 @@
+//Template literals:
+
+const age = 20;
+const fullName= "John Doe";
+
+// output: Hello Mr John Doe your age is: 20
+
+console.log("Hello Mr " + fullName+" your aage is: "+age);
+
+console.log(`Hello Mr ${fullName} your age is:${age}`);
+
+
+// Print sum of two numbers
+
+const a= 10;
+const b= 20;
+
+console.log(a+b);
+
+console.log(`Sum of ${a} and ${b} is ${a+b}`);
+
+
